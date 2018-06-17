dbb47de558e5df3aaf5a7a8477e5db73faf39ada
NoCodingNoLifee <sosidko.i@fpm.dnulive.dp.ua>
2018-06-17 12:22:05 +0300

    anddd Eugene

--- ABCEnjoy/MainActivity.cs
+++ ABCEnjoy/MainActivity.cs
@@ -10,6 +10,7 @@ namespace ABCEnjoy
         int count = 1;
         int b = 0;
         int c = 0;
+        int Eugene = 0;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
