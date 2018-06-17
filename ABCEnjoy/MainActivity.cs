b16f022401a7c3841ae997badcecd803a47a4c0c
Mike <hello.mikle@mail.ru>
2018-06-17 13:32:02 +0300

    upd

--- ABCEnjoy/MainActivity.cs
+++ ABCEnjoy/MainActivity.cs
@@ -7,11 +7,6 @@ namespace ABCEnjoy
     [Activity(Label = "ABCEnjoy", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
-        int b = 0;
-        int c = 0;
-        int Eugene = 0;
-        int taaj;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
